a298a818d909b48af06eadefaa1a126220f16795
BB <baptiste.bernard@utopiaweb.fr>
2015-02-15 20:39:08 +0100

    First commit

--- importer.sh
+++ importer.sh
@@ -6,18 +6,22 @@ DB_HOST="localhost"
 DB_PORT=3306
 DB_NAME="geonames"
 
+CONFIG_NAME="geonames-mysql-importer"
+
 BASE_URL="http://download.geonames.org/export/dump"
+
 
 usage() {
     PN=`basename "$0"`
     echo >&2 "Usage: $PN [OPTIONS] <action>"
     echo >&2 " Where <action>:"
+    echo >&2 "    config            Register database parameters (mysql user's password will be prompted to save the file)"
     echo >&2 "    init              Initialize geonames database"
     echo >&2 "    import            Import geonames database"
     echo >&2 "    update            Update database (usually should run daily by cron)"
-    echo >&2 " Options:"
+    echo >&2 "    empty             Empty tables"
+    echo >&2 " Config options:"
     echo >&2 "    -u <user>         Username to access database"
-    echo >&2 "    -p <password>     User password to access database"
     echo >&2 "    -h <host>         MySQL server address (default: $DB_HOST)"
     echo >&2 "    -r <port>         MySQL server port (default: $DB_PORT)"
     echo >&2 "    -n <database>     MySQL database name (default: $DB_NAME)"
@@ -26,11 +30,44 @@ usage() {
     exit 1
 }
 
+function checkConfig {
+    if [[ -n $(mysql_config_editor print --login-path=$CONFIG_NAME) ]]; then
+        return 1;
+    fi
+
+    return 0;
+}
+
+config() {
+    if [ -z "${DB_USERNAME}" ]; then
+        echo 'DB_USERNAME is required'
+        exit 1
+    fi
+
+    if [ -z "${DB_HOST}" ]; then
+        echo 'DB_HOST is required'
+        exit 1
+    fi
+
+    if [ -z "${DB_PORT}" ]; then
+        echo 'DB_PORT is required'
+        exit 1
+    fi
+
+    mysql_config_editor set --login-path=$CONFIG_NAME --host=$DB_HOST --port=$DB_PORT --user=$DB_USERNAME --password
+}
+
 init() {
+
+    if checkConfig; then
+        echo "You have to call 'config' action first to register database parameters"
+        exit
+    fi
+
     echo >&2 "Creating database $DB_NAME..."
-    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DROP DATABASE IF EXISTS $DB_NAME;"
-    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8;"
-    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD $DB_NAME < db_schema.sql
+    mysql --login-path=$CONFIG_NAME -Bse "DROP DATABASE IF EXISTS $DB_NAME;"
+    mysql --login-path=$CONFIG_NAME -Bse "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8;"
+    mysql --login-path=$CONFIG_NAME $DB_NAME < sql/db_schema.sql
 
     echo >&2 "Done"
     exit 0
@@ -42,11 +79,11 @@ import() {
 
     TODAY=`date +%F`
     echo >&2 "Creating directory $TODAY..."
-    mkdir -p "$TODAY"
+    mkdir -p "var/$TODAY"
 
-    cp -v continentCodes.txt "$TODAY/"
+    cp -v continentCodes.txt "var/$TODAY/"
 
-    cd "$TODAY"
+    cd "var/$TODAY"
 
     for FILE in $FILES_TO_DOWNLOAD; do
         echo >&2 "Downloading $FILE..."
@@ -58,7 +95,10 @@ import() {
     done
 
     echo >&2 "Importing geonames into database $DB_NAME..."
-    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD --local-infile=1 $DB_NAME < ../import.sql
+    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD --local-infile=1 $DB_NAME < ../../sql/import.sql
+
+    echo >&2 "Adding indexes on the tables in database $DB_NAME..."
+    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD $DB_NAME < ../../sql/add_indexes.sql
 
     echo >&2 "Done"
     cd ..
@@ -66,14 +106,24 @@ import() {
     exit 0
 }
 
+empty() {
+    echo "Emptying tables"
+    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD $DB_NAME < sql/truncate_tables.sql
+
+}
+
 update() {
-    YESTERDAY=`date --date='1 day ago' +%F`
+
+    case $(uname) in
+        Darwin|FreeBSD|NetBSD|DragonFLy) YESTERDAY=`date -v -1d +"%Y-%m-%d"` ;;
+        *) YESTERDAY=`date --date='1 day ago' +%F` ;;
+    esac
 
     FILES_TO_DOWNLOAD="modifications-$YESTERDAY.txt deletes-$YESTERDAY.txt alternateNamesModifications-$YESTERDAY.txt alternateNamesDeletes-$YESTERDAY.txt"
 
     TODAY=`date +%F`
     echo >&2 "Creating directory $TODAY..."
-    mkdir -p "$TODAY" && cd "$TODAY"
+    mkdir -p "var/$TODAY" && cd "var/$TODAY"
 
     for FILE in $FILES_TO_DOWNLOAD; do
         echo >&2 "Downloading $FILE..."
@@ -82,28 +132,29 @@ update() {
 
     echo >&2 "Deleting old names..."
     cat "deletes-$YESTERDAY.txt" | cut -f1 | while read ID; do
-        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM names WHERE name_id = $ID" $DB_NAME
+        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM geo_geoname WHERE id = $ID" $DB_NAME
     done
 
     echo >&2 "Applying changes to names..."
     cat "modifications-$YESTERDAY.txt" | cut -f1 | while read ID; do
-        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM names WHERE name_id = $ID" $DB_NAME
+        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM geo_geoname WHERE id = $ID" $DB_NAME
+        exit
     done
 
-    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD --local-infile=1 -Bse "LOAD DATA LOCAL INFILE 'modifications-$YESTERDAY.txt' INTO TABLE names CHARACTER SET 'utf8'" $DB_NAME
-
+    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD --local-infile=1 -Bse "LOAD DATA LOCAL INFILE 'modifications-$YESTERDAY.txt' INTO TABLE geo_geoname CHARACTER SET 'utf8'" $DB_NAME
+    exit
 
     echo >&2 "Deleting old alternate names..."
     cat "alternateNamesDeletes-$YESTERDAY.txt" | cut -f1 | while read ID; do
-        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM alternate_names WHERE alternate_name_id = $ID" $DB_NAME
+        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM geo_alternate_name WHERE id = $ID" $DB_NAME
     done
 
     echo >&2 "Applying changes to alternate names..."
     cat "alternateNamesModifications-$YESTERDAY.txt" | cut -f1 | while read ID; do
-        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM alternate_names WHERE alternate_name_id = $ID" $DB_NAME
+        mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD -Bse "DELETE FROM geo_alternate_name WHERE id = $ID" $DB_NAME
     done
 
-    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD --local-infile=1 -Bse "LOAD DATA LOCAL INFILE 'alternateNamesModifications-$YESTERDAY.txt' INTO TABLE alternate_names CHARACTER SET 'utf8'" $DB_NAME
+    mysql -h$DB_HOST -P$DB_PORT -u$DB_USERNAME -p$DB_PASSWORD --local-infile=1 -Bse "LOAD DATA LOCAL INFILE 'alternateNamesModifications-$YESTERDAY.txt' INTO TABLE geo_alternate_name CHARACTER SET 'utf8'" $DB_NAME
 
     echo >&2 "Done"
     cd ..
@@ -131,6 +182,8 @@ if [ $# -eq 1 ]; then
         init)   init ;;
         import) import ;;
         update) update ;;
+        empty) empty ;;
+        config) config ;;
         *)      usage ;;        # unknown command
     esac
 else
